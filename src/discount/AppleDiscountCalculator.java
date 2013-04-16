a4c9ce968ac057c196babadf7fb1b5bd06cbd77f
arkangelofkaos <arkangelofkaos@gmail.com>
2013-04-16 01:20:33 +0100

    renamed predicate

--- src/discount/AppleDiscountCalculator.java
+++ src/discount/AppleDiscountCalculator.java
@@ -11,11 +11,11 @@ public class AppleDiscountCalculator extends ItemDiscountCalculator {
     public static final long APPLE_DISCOUNT = 100L;
     public static final long NUMBER_OF_APPLES_PER_DISCOUNT = 4L;
 
-    private static final Predicate<? super Item> ITEM_FILTER_PREDICATE =
+    private static final Predicate<? super Item> APPLE_FILTER_PREDICATE =
             x -> x == Item.APPLE || x == Item.POMME || x == Item.MELE;
 
     AppleDiscountCalculator() {
         super(APPLE_DISCOUNT, NUMBER_OF_APPLES_PER_DISCOUNT,
-                ITEM_FILTER_PREDICATE);
+                APPLE_FILTER_PREDICATE);
     }
 }
